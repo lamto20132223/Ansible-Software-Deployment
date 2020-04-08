dae466158cec4fdbe07d3125ee31a489abf8c64e
lamtv10 <lamtv10@viettel.com.vn>
2020-04-08 17:03:37 +0700

    update _ task _index

--- controllers/assign_role/routes.py
+++ controllers/assign_role/routes.py
@@ -9,7 +9,7 @@ from assets import *
 import sys
 import oyaml as yaml
 from sqlalchemy.exc import IntegrityError
-import libs.ansible.runner.Runner as Runner
+from libs.ansible.runner import Runner
 from flask_restplus import Api, Resource
 import json
 import ast
