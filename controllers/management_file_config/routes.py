1b6f04b38a0654a4df7f73213a6e1561770f9739
lamtv10 <lamtv10@viettel.com.vn>
2020-05-07 11:15:35 +0700

    abcdev123456

--- controllers/management_file_config/routes.py
+++ controllers/management_file_config/routes.py
@@ -12,6 +12,29 @@ mod = Blueprint('management_file_config', __name__,
 
 
 
+
+
+
+
+
+# def convert_cfg_to_yaml():
+#     return "INCOMMING"
+#
+# def convert_yaml_to_cfg():
+#     return "INCOMMING"
+#
+#
+# def add_file_config_to_manager():
+#
+#
+# def list_all_file_config():
+#     get,set,download
+#
+# def edit_one_file_config():
+
+
+
+
 @mod.route('/configs/<string:host_id>', methods=['GET'])
 def get_all_config_in_host(host_id):
     return "INCOMMING"
@@ -89,6 +112,115 @@ def rollback_file_config():
     if not (request.args.get('file_config_id')):
         abort(400)
     return "Update File Config From DB to Server"
+
+
+
+
+@mod.route('/tests/test1')
+def rest_assurate_test():
+    jsonx = {
+        "lotto":{
+         "lottoId":5,
+         "winning-numbers":[2,45,34,23,7,5,3],
+         "winners":[{
+           "winnerId":23,
+           "numbers":[2,45,34,23,3,5]
+         },{
+           "winnerId":54,
+           "numbers":[52,3,12,11,18,22]
+         }]
+        }
+        }
+
+    return jsonx,200
+
+
+@mod.route('/tests/test2')
+def rest_assurate_test2():
+    jsonx = {
+   "store":{
+      "book":[
+         {
+            "author":"Nigel Rees",
+            "category":"reference",
+            "price":8.95,
+            "title":"Sayings of the Century"
+         },
+         {
+            "author":"Evelyn Waugh",
+            "category":"fiction",
+            "price":12.99,
+            "title":"Sword of Honour"
+         },
+         {
+            "author":"Herman Melville",
+            "category":"fiction",
+            "isbn":"0-553-21311-3",
+            "price":8.99,
+            "title":"Moby Dick"
+         },
+         {
+            "author":"J. R. R. Tolkien",
+            "category":"fiction",
+            "isbn":"0-395-19395-8",
+            "price":22.99,
+            "title":"The Lord of the Rings"
+         }
+      ]
+   }
+}
+
+    return jsonx,200
+
+
+
+@mod.route('/tests/test3')
+def rest_assurate_test3():
+    jsonx = [
+          {
+              "id": 2,
+              "name": "An ice sculpture",
+              "price": 12.50,
+              "tags": ["cold", "ice"],
+              "dimensions": {
+                  "length": 7.0,
+                  "width": 12.0,
+                  "height": 9.5
+              },
+              "warehouseLocation": {
+                  "latitude": -78.75,
+                  "longitude": 20.4
+              }
+          },
+          {
+              "id": 3,
+              "name": "A blue mouse",
+              "price": 25.50,
+                  "dimensions": {
+                  "length": 3.1,
+                  "width": 1.0,
+                  "height": 1.0
+              },
+              "warehouseLocation": {
+                  "latitude": 54.4,
+                  "longitude": -32.7
+              }
+          }
+      ]
+    return jsonify(jsonx),200
+
+
+
+
+@mod.route('/tests/test5', methods = ['GET', 'POST'])
+def rest_assurate_test5():
+    if request.method== 'POST':
+        jsonx = request.json
+        return jsonify(jsonx),200
+    else:
+        json = {'message':'Hello From Flask'}
+        return jsonify(json), 200
+
 
 
 
