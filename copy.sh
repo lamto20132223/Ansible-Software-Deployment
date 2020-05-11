d24ec078de65e31897f1040a684616f5e684487e
lamtv10 <lamtv10@viettel.com.vn>
2020-05-11 17:35:17 +0700

    Update ansible Gather Fact

--- copy.sh
+++ copy.sh
@@ -4,3 +4,5 @@ mkdir /root/app/bk
 \cp -r /root/app/static/ansible/inventory /root/app/bk/
 
 \cp -r /root/app/static/ansible/group_vars_template /root/app/bk
+
+\cp /root/app/static/ansible/ansible.cfg /etc/ansible/ansible.cfg
